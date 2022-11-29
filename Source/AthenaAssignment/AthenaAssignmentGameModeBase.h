eab2362626b5e41b4eeac8ee838d1d24b08bbdc1
David A Roberts <d@vidr.cc>
2022-11-29 14:15:09 +1000

    Step 1: rough outback landscape

---
+++ Source/AthenaAssignment/AthenaAssignmentGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AthenaAssignmentGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ATHENAASSIGNMENT_API AAthenaAssignmentGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
